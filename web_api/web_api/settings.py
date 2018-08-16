e316a668efbdf368ad49068fa60ec46985f434e2
William Strong <william.strong@me.com>
2018-08-15 20:32:57 -0700

    Add aws keys

--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -48,6 +48,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'aforest-web'
 AWS_S3_REGION_NAME = 'us-west-1'  # e.g. us-east-2
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
