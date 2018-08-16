c13925270655afa9d74f9e682d6e2a34d3dd06f7
William Strong <william.strong@me.com>
2018-08-16 12:20:54 -0700

    Turn debug false as default

--- web_api/project/settings.py
+++ web_api/project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = False
+# os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
